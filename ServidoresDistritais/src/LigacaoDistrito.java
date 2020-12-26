8be77fd003f943f94e2be3c0c125b3c6841cbc79
rvcarvalho99 <33839479+rvcarvalho99@users.noreply.github.com>
2020-12-26 18:10:29 +0000

    ServidoresInitializer
    .

--- ServidoresDistritais/src/LigacaoDistrito.java
+++ ServidoresDistritais/src/LigacaoDistrito.java
@@ -12,15 +12,16 @@ public class LigacaoDistrito {
     private ServidorDistrital sd;
 
     public static void main(String args[]) {
+        System.out.println(args[0]);
         try (ZContext context = new ZContext()) {
             //  Socket to send messages on
-            String distritos[] = {"Lisboa", "Porto", "Braga", "Setúbal", "Aveiro", "Faro", "Leiria", "Coimbra", "Santarém", "Viseu", "Madeira", "Acores", "Viana Do Castelo", "Vila Real", "Castelo Branco", "Évora", "Guarda", "Beja", "Bragança", "Portalegre"};
+            String distritos[] = {"Lisboa", "Porto", "Braga", "Setubal", "Aveiro", "Faro", "Leiria", "Coimbra", "Santaréqm", "Viseu", "Madeira", "Acores", "Viana Do Castelo", "Vila Real", "Castelo Branco", "Évora", "Guarda", "Beja", "Bragança", "Portalegre"};
             System.out.println(distritos.length);
             BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Qual distrito pretende?\n" + Arrays.toString(distritos));
 
             ZMQ.Socket replyer = context.createSocket(SocketType.REP);
-            replyer.connect("tcp://127.0.0.1:5555" + Integer.parseInt(input.readLine()));
+            replyer.connect("tcp://127.0.0.1:" + args[0] + + Integer.parseInt(input.readLine()));
             ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
             //subscriber.connect(F.Server.BOUNDED_ADDRESS);
 
---
+++ ServidoresDistritais/src/ServidoresInitializer.java
@@ -0,0 +1,19 @@
+public class ServidoresInitializer {
+
+    public static void main(String args[]) {
+        int port = 5555;
+        for(int i = 0; i<20;i++)
+        {
+            LigacaoDistrito distrito = new LigacaoDistrito();
+            String[] arguments = new String[] {String.valueOf(port+i)};
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    distrito.main(arguments);
+                }
+            });
+            thread.start();
+        }
+    }
+
+}
